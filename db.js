bffad4c736fe3ba8da29ed1d0dd83972ac7ba26d
masud467 <masudpfrnt@gmail.com>
2025-01-07 20:21:51 +0600

    change timeout.

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 const sqlite3 = require("sqlite3").verbose();
 
 // Connect to the database
-const db = new sqlite3.Database("./dua_main.sqlite", (err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err.message);
-  } else {
-    console.log("Connected to the SQLite database.");
+const db = new sqlite3.Database(
+  "./dua_main.sqlite",
+  {
+    timeout: 10000, // 10 second timeout
+  },
+  (err) => {
+    if (err) {
+      console.error("Error connecting to the database:", err.message);
+    } else {
+      console.log("Connected to the SQLite database.");
+    }
   }
-});
+);
 
 module.exports = db;
